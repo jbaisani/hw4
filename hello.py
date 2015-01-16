d150dea565b243d97703cd21455192e8b8cfabf4
jbaisani <janardhanareddy.b@gmail.com>
2015-01-15 20:40:24 -0800

    adding new file

---
+++ hello.py
@@ -0,0 +1,6 @@
+# please enter the commit message for your changes . Lines Start
+# with '#' will be ignored, and an empty message aborts the commit
+# on branch master
+# changes to commited 
+# modified : Hello.py
+print 'jana'
